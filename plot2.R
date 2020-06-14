ee1d1f70704e439f322da32e551fc5956d81190d
ojasvibhasin <65826071+ojasvibhasin@users.noreply.github.com>
2020-06-14 18:16:06 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,19 @@
+library("data.table")
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/ojasvibhasin/project")
+#Reads in data from file
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
+# Prevents Scientific Notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+# Making a POSIXct date capable of being filtered 
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+# Filter Dates 
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot2.png", width=480, height=480)
+
+## Plot 2
+plot(x = powerDT[, dateTime]
+     , y = powerDT[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
